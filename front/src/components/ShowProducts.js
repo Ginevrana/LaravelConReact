3b397a7cf349c3e93066e36ac7e60ce8fc6f5830
Florencia Teresita Sanchez <florenciateresita@gmail.com>
2023-05-15 17:47:07 -0300

    Se arregla app.js

--- front/src/components/ShowProducts.js
+++ front/src/components/ShowProducts.js
@@ -1,6 +1,5 @@
 import React, {useEffect,useState} from 'react'
 import axios from 'axios'
-
 import {Link} from 'react-router-dom'
 
 // Se define constante desde donde se alimentan datos del back
@@ -11,9 +10,6 @@ const ShowProducts = () => {
     // Arreglo. Constante llamada products y su función setProducts
     const [products,setProducts] = useState([])
     
-    // Contador con variable que guardaría el estado de ese contador que inicia en 0
-    const [count,setCount] = useState(0)
-    
     // Despues de renderizarse (Tabla) el componente entra en acción
     useEffect( ()=>{
         getAllProducts()
@@ -23,7 +19,8 @@ const ShowProducts = () => {
         // Para traer la información del back
         const response = await axios.get(`${endpoint}/products`)
         // Para actualizar estado actual
-        setProducts(response)
+        setProducts(response.data)
+        console.log(response.data);
     }
 
     const deleteProduct = async (id) => {
@@ -34,7 +31,35 @@ const ShowProducts = () => {
     }
 
   return (
-    <div>Tabla</div>
+    <div>
+        <div className='d-grid gap-2'>
+            <link to="/create" className='btn btn-success btn-lg mt-2 mb-2 text-white'>Create</link>
+        </div>
+
+        <table className='table table-striped'>
+            <thead className='bg-primary text-white'>
+                <tr>
+                    <th>Description</th>
+                    <th>Price</th>
+                    <th>Stock</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                { products.map((product)=>(
+                    <tr key={product.id}>
+                        <td>{product.description}</td>
+                        <td>{product.price}</td>
+                        <td>{product.stock}</td>
+                        <td>
+                            <Link to={`/edit/${product.id}`} className='btn btn-warning'>Edit</Link>
+                            <button onClick={()=>deleteProduct(product.id)} className='btn btn-danger'>Delete</button>
+                        </td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    </div>
   )
 }
 
